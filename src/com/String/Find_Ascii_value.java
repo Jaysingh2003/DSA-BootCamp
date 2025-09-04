d17fa89e43ac361fcf0b4f2d6b5730dd07aebc68
Jaysingh Patel <153704686+Jaysingh2003@users.noreply.github.com>
2025-09-04 21:07:34 +0530

    Recursion subsequence

---
+++ src/com/String/Find_Ascii_value.java
@@ -0,0 +1,10 @@
+package com.String;
+
+public class Find_Ascii_value {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+
+        System.out.println(ch+0 );//to find the ascii value of a character we can add 0 to it
+    }
+}
--- src/com/recursion_InString/possible_Subset.java
+++ src/com/recursion_InString/possible_Subset.java
@@ -4,37 +4,55 @@ import java.util.ArrayList;
 
 public class possible_Subset {
     public static void main(String[] args) {
-        String str="abc";
+        String str = "abc";
         subset("", str);
         System.out.println(subsetOfString("", str));
+        System.out.println(subsetWithASCII("", str));
 
     }
-    static void subset(String p,String up){//p is the processed part and up is the unprocessed part
-        if(up.isEmpty()){
+
+    static void subset(String p, String up) {//p is the processed part and up is the unprocessed part
+        if (up.isEmpty()) {
             System.out.println(p);//when the unprocessed part is empty, print the processed part
             return;
         }
-        char ch=up.charAt(0);
+        char ch = up.charAt(0);
         subset(p, up.substring(1));//exclude the character
-        subset(p+ch, up.substring(1));//include the character
+        subset(p + ch, up.substring(1));//include the character
 
 
     }
 
     //if we have to return the subset in the form of an arraylist then we can do it like this
-     static ArrayList<String> subsetOfString(String p, String up){
-          if(up.isEmpty()){
-                ArrayList<String> list=new ArrayList<>();
-                list.add(p);
-                return list;
+    static ArrayList<String> subsetOfString(String p, String up) {
+        if (up.isEmpty()) {
+            //after the up is empty all the element are in p so we have to return p in the form of an arraylist
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);//add p to the list
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subsetOfString(p, up.substring(1));//exclude the character
+        ArrayList<String> right = subsetOfString(p + ch, up.substring(1));//include the character
+        left.addAll(right);
+        return left;//we can return any one of them because both contains all the subset
+    }
 
-          }
-            char ch=up.charAt(0);
-
-            ArrayList<String> left=subsetOfString(p, up.substring(1));//exclude the character
-            ArrayList<String> right=subsetOfString(p+ch, up.substring(1));//include the character
-            left.addAll(right);
-            return left;
-     }
+    //fint the ascii value of the subset
+    static ArrayList<String> subsetWithASCII(String p, String up) {
+        if (up.isEmpty()) {
+            //after the up is empty all the element are in p so we have to return p in the form of an arraylist
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);//add p to the list
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> first = subsetWithASCII(p, up.substring(1));//exclude the character
+        ArrayList<String> second = subsetWithASCII(p + ch, up.substring(1));//include the character
+        ArrayList<String> third = subsetWithASCII(p + (ch + 0), up.substring(1));//include the ascii value of the character
+         first.addAll(second);
+         first.addAll(third);
+        return first ;//we can return any one of them because both contains all the subset
+    }
 
 }
