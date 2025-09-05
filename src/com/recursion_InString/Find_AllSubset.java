89b6f1cefefb4cf07e9a94bc057dd5b020df326c
Jaysingh Patel <153704686+Jaysingh2003@users.noreply.github.com>
2025-09-06 00:30:42 +0530

    Recursion powerset

--- src/com/recursion_InString/Find_AllSubset.java
+++ src/com/recursion_InString/Find_AllSubset.java
@@ -1,12 +1,18 @@
 package com.recursion_InString;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 // https://leetcode.com/problems/subsets/description/
 public class Find_AllSubset {
     public static void main(String[] args) {
         int[] num = {1,2,3};
-        System.out.println(findSubsets(num));
+//        System.out.println(findSubsets(num));
+        List<List<Integer>> list = findSubsets(num);
+        for (List<Integer> l : list) {
+            System.out.println(l);
+        }
+
 
     }
     static List<List<Integer>> findSubsets(int[] num){
@@ -23,4 +29,36 @@ public class Find_AllSubset {
         return outer;
 
     }
+
+    // Time Complexity: O(N * 2^N)
+    // Space Complexity: O(N * 2^N)
+
+    //if the duplicate elment is poresent
+
+    static List<List<Integer>> findSubsetsDuplicate(int[] nums){
+        List<List<Integer>> outer = new ArrayList<>();
+        Arrays.sort(nums);
+       outer.add(new ArrayList<>());
+       int start = 0;
+       int end = 0;
+       for(int i=0; i<nums.length; i++){
+             start = 0;
+              //if current and previous element are same, new subsets are only formed from the subsets
+                //added in the previous step
+           if(i>0  && nums[i] == nums[i-1]){
+                start = end + 1;
+              }
+              end = outer.size() - 1;
+           }
+              int size = outer.size();
+                for(int j=start; j<size; j++){
+                    List<Integer> internal = new ArrayList<>(outer.get(j));
+                    internal.add(nums[j]);
+                    outer.add(internal);
+       }
+        return outer;
+
+
+    }
+
 }
