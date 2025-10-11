56aac8364228ecc45da87f743a4dfdc9019a47bc
Jaysingh Patel <153704686+Jaysingh2003@users.noreply.github.com>
2025-10-11 23:42:01 +0530

    file handling to track the exception

--- src/com/filhandling/Main.java
+++ src/com/filhandling/Main.java
@@ -1,22 +1,53 @@
 package com.filhandling;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        try(InputStreamReader isr = new InputStreamReader(System.in)){/// creates a reader to read characters from the console.
-            System.out.println("enter any character:");
-            int letter= isr.read();///reads the first character as an integer (ASCII value).
-            while(isr.ready()){/// eads the first character as an integer (ASCII value).
-                System.out.println((char)letter);///prints the character.
-                letter= isr.read();///reads the next one.
+//        try(InputStreamReader isr = new InputStreamReader(System.in)){/// creates a reader to read characters from the console.
+//            System.out.println("enter any character:");
+//            int letter= isr.read();///reads the first character as an integer (ASCII value).
+//            while(isr.ready()){/// eads the first character as an integer (ASCII value).
+//                System.out.println((char)letter);///prints the character.
+//                letter= isr.read();///reads the next one.
+//            }
+//           /// isr.close();
+//            System.out.println();
+//
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+
+        /// using the filereader
+        try (FileReader fr = new FileReader("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/note.txt")) {/// creates a reader to read characters from the console.
+
+            int letter = fr.read();///reads the first character as an integer (ASCII value).
+            while (fr.ready()) {/// eads the first character as an integer (ASCII value).
+                System.out.println((char) letter);///prints the character.
+                letter = fr.read();///reads the next one.
             }
-           /// isr.close();
+            /// isr.close();
             System.out.println();
 
-        }catch (IOException e){
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+/// buffers the input so that characters are read in larger chunks rather than one at a time.
+   /// if we want to read from console
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in))) {/// it will take the data from the keyboard
+            /// buffer to char stream and then read the char stream
+            System.out.println(bf.readLine());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+/// if we want to read from the file
+        try (BufferedReader bf = new BufferedReader(new FileReader("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/note.txt"))) {
+           while (bf.ready()){/// checks if the file has more characters to read.
+               System.out.println(bf.readLine());
+           }
+        } catch (IOException e) {//for the exception
             System.out.println(e.getMessage());
         }
 
