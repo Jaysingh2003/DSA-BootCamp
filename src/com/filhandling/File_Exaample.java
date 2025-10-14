e110f6c761fadc6955d2f245a7d8dbf7caceb796
Jaysingh Patel <153704686+Jaysingh2003@users.noreply.github.com>
2025-10-15 02:44:45 +0530

    file creating , writing, appending, deleting

--- src/com/filhandling/File_Exaample.java
+++ src/com/filhandling/File_Exaample.java
@@ -1,25 +1,45 @@
 package com.filhandling;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 
 public class File_Exaample {
     public static void main(String[] args) {
         /// create a file
-//        try {
-//            File file = new File("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/file1.txt");//Creates a new File instance
-//            file.createNewFile();///Atomically creates a new, empty file named by this abstract pathname if and only if a file with this name does not yet exist
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        try {
+            File file = new File("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/file1.txt");//Creates a new File instance
+            file.createNewFile();///Atomically creates a new, empty file named by this abstract pathname if and only if a file with this name does not yet exist
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        /// write in file
+        /// write into file
         try{
             FileWriter fw= new FileWriter("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/file1.txt");
             fw.write("मत्तः परतरं नान्यत्किञ्चिदस्ति धनञ्जय।\n" +
                     "मयि सर्वमिदं प्रोतं सूत्रे मणिगणा इव।।7.7।।");
             fw.close();
         }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        /// reading from the file -> dirctlt on the console
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/file1.txt"))) {
+            while (br.ready()) {/// checks if the file has more characters to read.
+                System.out.println(br.readLine());
+            }
+        } catch (IOException e) {//for the exception
+            System.out.println(e.getMessage());
+        }
+
+        /// delete file
+        try {
+            File file = new File("/Users/jaysingh/DSA/DSA bootcamp by kunal/src/com/filhandling/random.txt");//Creates a new File instance
+            file.createNewFile();// creates a new file
+            file.delete();///delete the file at a give directory
+            if(file.delete()){
+                System.out.println(file.getName()+" is deleted");
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
