e61da188bfea0827eab7ba8f43aaef3af90f9278
Jaysingh Patel <153704686+Jaysingh2003@users.noreply.github.com>
2025-10-23 12:30:54 +0530

    RightSideView traversal

---
+++ src/com/tree/RightSideView.java
@@ -0,0 +1,40 @@
+package com.tree;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//https://leetcode.com/problems/binary-tree-right-side-view/
+public class RightSideView {
+    public List<Double> rightSideView(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result; // handle empty tree
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                /// as the last node in the level we add its value to the result
+                if (i == levelSize - 1) {
+                    result.add((double) current.val);
+                }
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+
+        }
+        return result;
+
+    }
+}
