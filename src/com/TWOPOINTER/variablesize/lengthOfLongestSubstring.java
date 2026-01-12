b3866994d9aab378b1721c4d0c9712f7f8b6b40e
Jaysingh Patel <jay229413@gmail.com>
2026-01-12 15:51:12 +0530

    productExceptSelf

---
+++ src/com/TWOPOINTER/variablesize/lengthOfLongestSubstring.java
@@ -0,0 +1,29 @@
+package com.TWOPOINTER.variablesize;
+
+import java.util.HashMap;
+
+public class lengthOfLongestSubstring {
+    public int lengthflongestsubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+
+            map.put(ch, right);
+            maxLen = Math.max(maxLen, right - left + 1);//we comapere with th window size nopt the k like in previous problems
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        lengthOfLongestSubstring solution = new lengthOfLongestSubstring();
+        String testString = "abcabcbb";
+        int result = solution.lengthflongestsubstring(testString);
+        System.out.println("Length of longest substring without repeating characters: " + result);
+    }
+}
