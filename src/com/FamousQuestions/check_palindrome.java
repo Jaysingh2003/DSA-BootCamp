3fd6376c5fdf4f30205fb533c7ca68a458536bbf
Jaysingh Patel <jay229413@gmail.com>
2026-01-22 16:35:26 +0530

    engineering_digest_cheatsheet

---
+++ src/com/FamousQuestions/check_palindrome.java
@@ -0,0 +1,36 @@
+package com.FamousQuestions;
+
+public class check_palindrome {
+
+    // Function to check if a string is a palindrome
+    static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+    //To check if the array is palindrome or not
+    static boolean isArrayPalindrome(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
---
+++ src/com/FamousQuestions/string/ComapreString.java
@@ -0,0 +1,18 @@
+package com.FamousQuestions.string;
+
+public class ComapreString {
+    public static boolean isequal(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            return str1 == str2;
+        }
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
