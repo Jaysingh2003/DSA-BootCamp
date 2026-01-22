ae48efa8b4c18634c4c0c515747d845c5baa8168
Jaysingh Patel <jay229413@gmail.com>
2026-01-22 20:16:45 +0530

    engineering_digest_cheatsheet

---
+++ src/com/FamousQuestions/Linear_search.java
@@ -0,0 +1,13 @@
+package com.FamousQuestions;
+
+public class Linear_search {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
