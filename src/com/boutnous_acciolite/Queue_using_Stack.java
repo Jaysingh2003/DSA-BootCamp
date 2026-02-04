effc229bb2ba83001c41b606c8e36cd6754d2d0e
Jaysingh Patel <jay229413@gmail.com>
2026-02-04 17:19:53 +0530

    Graph data structure

---
+++ src/com/boutnous_acciolite/Queue_using_Stack.java
@@ -0,0 +1,52 @@
+package com.boutnous_acciolite;
+
+import java.util.Stack;
+
+public class Queue_using_Stack {
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    // Enqueue operation
+    //complexity O(n)
+
+    // ther are three step
+    // copy the elements from s1 to s2
+    // add the new element to s1
+    // copy back all the elements from s2 to s1
+    public void enqueue(int data) {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(data);
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    // Dequeue operation
+    //complexity O(1)
+    public int dequeue() {
+        if (s1.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return s1.pop();
+    }
+
+    //top operation
+    //complexity O(1)
+    public int top() {
+        if (s1.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return s1.peek();
+    }
+
+    // Check if the queue is empty
+    //complexity O(1)
+    public boolean isEmpty() {
+        return s1.isEmpty();
+    }
+}
